4ea18daa5fcd0a2f114511a2c79f469384fdfd90
Anak Mirasing <thaihooligan@hotmail.com>
2015-03-17 21:38:46 +0700

    Initial Pod, Project Structure

---
+++ FirePizza/Controller/MainMenuViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainMenuViewController.swift
+//  FirePizza
+//
+//  Created by Anak Mirasing on 3/17/2558 BE.
+//  Copyright (c) 2558 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("MainMenuViewController")
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'FirePizza' do
+pod 'Firebase'
+end
+
+target 'FirePizzaTests' do
+
+end
+
